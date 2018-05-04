93f99aecc41cf23ea168b7a9cc8dcc490edbc85c
jlpereira8 <jlpereira355@gmail.com>
2018-05-04 16:17:21 -0600

    Crear Examen

--- Lab2_JoseLuisPereira/src/lab2_joseluispereira/Lab2_JoseLuisPereira.java
+++ Lab2_JoseLuisPereira/src/lab2_joseluispereira/Lab2_JoseLuisPereira.java
@@ -22,53 +22,56 @@ public class Lab2_JoseLuisPereira {
     static Scanner lea = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ArrayList lista=new ArrayList();
+        ArrayList lista = new ArrayList();
         ArrayList<Alumnos> user = new ArrayList<>();
+
         int des1 = 0, des2 = 0, des3 = 0, des4 = 0, des5 = 0, des6 = 0;
+        String resp1, preg1;
         Alumnos actual = null;
-       while (des2 != 4) {
-        System.out.println("*******MENU*******");
-        System.out.println("1) Administracion");
-        System.out.println("2) Crear Cuenta");
-        System.out.println("3) LogIn");
-        System.out.println("Ingrese su opcion");
-        int des = lea.nextInt();
-        switch (des) {
+        while (des2 != 4) {
+            System.out.println("*******MENU*******");
+            System.out.println("1) Administracion");
+            System.out.println("2) Crear Cuenta");
+            System.out.println("3) LogIn");
+            System.out.println("Ingrese su opcion");
+            int des = lea.nextInt();
+            switch (des) {
                 case 1:
                     Admin();
+                    break;
                 case 2:
                     System.out.println("Ingrese nombre");
-                    String nom=lea.next();
+                    String nom = lea.next();
                     System.out.println("Ingrese numero de cuenta");
-                    String cuenta=lea.next();
+                    String cuenta = lea.next();
                     System.out.println("Ingrese fecha");
-                    String fe=lea.next();
+                    String fe = lea.next();
                     System.out.println("Ingrese su edad");
-                    int ed=lea.nextInt();
+                    int ed = lea.nextInt();
                     System.out.println("Ingrese su Ciudad ");
-                    String ci=lea.next();
+                    String ci = lea.next();
                     System.out.println("Ingrese su Nacionalidad");
-                    String na=lea.next();
+                    String na = lea.next();
                     System.out.println("Ingrese su ID");
-                    String ID=lea.next();
+                    String ID = lea.next();
                     System.out.println("Ingrese su nombre de Usario");
-                    String us=lea.next();
+                    String us = lea.next();
                     System.out.println("Ingrese su contrasena");
-                    String pas=lea.next();
-                    user.add(new Alumnos(nom,cuenta,fe,ed,ci,na,ID,us,pas));
+                    String pas = lea.next();
+                    user.add(new Alumnos(nom, cuenta, fe, ed, ci, na, ID, us, pas));
                     break;
                 case 3:
-                   System.out.println("Ingrese usuario: ");
+                    System.out.println("Ingrese usuario: ");
                     String CompareUs = lea.next();
                     System.out.println("Ingrese contraseña:");
                     String contra = lea.next();
                     for (Alumnos x : user) {
                         if (CompareUs.equals(x.getUser()) && contra.equals(x.getPass())) {
                             cc();
-                        }else{
+                        } else {
                             System.out.println("Usuario no valido");
                         }
-                    }                
+                    }
                     break;
                 case 4:
                     des2 = 4;
@@ -78,7 +81,10 @@ public class Lab2_JoseLuisPereira {
 
     }
 
-    public static void Admin() { 
+    public static void Admin() {
+        ArrayList<String> resps = new ArrayList<>();
+        ArrayList<String> pregs = new ArrayList<>();
+        String es, es2;
         System.out.println("1) Crear Examen");
         System.out.println("2) Revisar Examen");
         System.out.println("3) Ver notas");
@@ -87,7 +93,28 @@ public class Lab2_JoseLuisPereira {
         int red = lea.nextInt();
         switch (red) {
             case 1:
-
+                System.out.println("Ingrese el numero de preguntas de su examen ");
+                int max = lea.nextInt();
+                for (int i = 0; i < max * 2; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println("Ingrese pregunta");
+                        es = lea.next();
+                        pregs.add(es);
+                    } else {
+                        System.out.println("Ingrese respuesta");
+                        es2 = lea.next();
+                        resps.add(es2);
+                    }
+                }
+                System.out.println("PREGUNTAS");
+                for (int i = 1; i < pregs.size()+1; i++) {
+                    System.out.println(i+") " + pregs.get(i) + "]");
+                }
+                System.out.println("RESPUESTAS");
+                for (int i = 1; i < resps.size()+1; i++) {
+                    System.out.println(i+") " + resps.get(i) + "]");
+                }
+                System.out.println("");
                 break;
             case 2:
 
